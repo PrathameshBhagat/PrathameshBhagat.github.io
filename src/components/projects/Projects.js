95b64cda692ce011cf96f92a289f15678fa9e4a8
Prathamesh Bhagat <prathameshbhagat0608@gmail.com>
2024-03-30 16:59:58 +0530

    Projects component imported.

---
+++ src/components/projects/Projects.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import './Projects.css'
+const Projects = () => {
+  return (
+    <>
+        <div className="Projects" >
+            <div className="project" >
+                <div>
+                    <p>Year</p>
+                </div>
+
+                <div>
+                    <img src="https://via.placeholder.com/400" />
+                </div>
+
+                <div>
+                    <span>Type</span>
+                    <h3>Title</h3>
+                </div>
+            </div>
+            <div className="project" >
+                <div>
+                    <p>Year</p>
+                </div>
+
+                <div>
+                    <img src="https://via.placeholder.com/400" />
+                </div>
+
+                <div>
+                    <span>Type</span>
+                    <h3>Title</h3>
+                </div>
+            </div>
+            <div className="project" >
+                <div>
+                    <p>Year</p>
+                </div>
+
+                <div>
+                    <img src="https://via.placeholder.com/400" />
+                </div>
+
+                <div>
+                    <span>Type</span>
+                    <h3>Title</h3>
+                </div>
+            </div>
+        </div>
+
+        <div className="Projects" >    
+            <div className="project" >
+                <div>
+                    <p>Year</p>
+                </div>
+
+                <div>
+                    <img src="https://via.placeholder.com/400" />
+                </div>
+
+                <div>
+                    <span>Type</span>
+                    <h3>Title</h3>
+                </div>
+            </div>
+            <div className="project" >
+                <div>
+                    <p>Year</p>
+                </div>
+
+                <div>
+                    <img src="https://via.placeholder.com/400" />
+                </div>
+
+                <div>
+                    <span>Type</span>
+                    <h3>Title</h3>
+                </div>
+            </div>
+            <div className="project" >
+                <div>
+                    <p>Year</p>
+                </div>
+
+                <div>
+                    <img src="https://via.placeholder.com/400" />
+                </div>
+
+                <div>
+                    <span>Type</span>
+                    <h3>Title</h3>
+                </div>
+            </div>    
+        </div>
+    </>)
+}
+
+export default Projects
