42bdf0bcd08e8749c44e72a8be6e4b7afdb3284b
Prathamesh Bhagat <prathameshbhagat0608@gmail.com>
2024-03-31 12:04:04 +0530

    Projects aligned properly for all devices

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -3,6 +3,7 @@ import './Projects.css'
 const Projects = () => {
   return (
     <>
+        <h1  style={{margin:"15% auto 5%",backgroundColor:"grey",fontWeight:"bold"}}>Projects</h1>
         <div className="Projects" >
             <div className="project" >
                 <div>
@@ -32,22 +33,7 @@ const Projects = () => {
                     <h3>Title</h3>
                 </div>
             </div>
-            <div className="project" >
-                <div>
-                    <p>Year</p>
-                </div>
-
-                <div>
-                    <img src="https://via.placeholder.com/400" />
-                </div>
-
-                <div>
-                    <span>Type</span>
-                    <h3>Title</h3>
-                </div>
-            </div>
         </div>
-
         <div className="Projects" >    
             <div className="project" >
                 <div>
@@ -63,6 +49,22 @@ const Projects = () => {
                     <h3>Title</h3>
                 </div>
             </div>
+            <div className="project" >
+                <div>
+                    <p>Year</p>
+                </div>
+
+                <div>
+                    <img src="https://via.placeholder.com/400" />
+                </div>
+
+                <div>
+                    <span>Type</span>
+                    <h3>Title</h3>
+                </div>
+            </div>
+        </div>
+        <div className="Projects" >
             <div className="project" >
                 <div>
                     <p>Year</p>
