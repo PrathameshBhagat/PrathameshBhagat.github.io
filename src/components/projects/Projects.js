63c9c0603cca61fdc7830078bcb328c1463294e4
Prathamesh Bhagat <prathameshbhagat0608@gmail.com>
2024-07-11 20:00:41 +0530

    Projects rearranged

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -31,15 +31,15 @@ const Projects = () => {
         </h1>
 
         <div className="project-row" >
-            <Project title={"Drone"} image={"/drone.png"} language={"C++ & JS"} year={"2023-24"} href="https://github.com/PrathameshBhagat/Drone-Branches-Project"/>
+            <Project title={"Android"} image={"/ofyWellness.jpg"} language={"Java & XML"} year={"2024"} href="https://github.com/PrathameshBhagat/OFY-Wellness"/>
             <Project title={"Converter"} image={"/currencyConverter.png"} language={"Java & Spring"} year={"2024"} href="https://github.com/PrathameshBhagat/Spring-Earning-Revenue-Converter"/>
         </div>
         <div className="project-row" >
-            <Project title={"Stocks"} image={"/stock.jpg"} language={"Python & Java"} year={"2021-22"} href="https://github.com/PrathameshBhagat/Pratice-Files/tree/main/PPLab"/>
-            <Project title={"Android"} image={"/ofyWellness.jpg"} language={"C++ & JS"} year={"2024"} href="https://github.com/PrathameshBhagat/OFY-Wellness"/>
+            <Project title={"Docs"} image={"/documentation.png"} language={"JS & PHP"} year={"2022"} href="https://github.com/PrathameshBhagat/Documentation-Project"/>
+            <Project title={"Drone"} image={"/drone.png"} language={"C++ & JS"} year={"2023-24"} href="https://github.com/PrathameshBhagat/Drone-Branches-Project"/>
         </div>
         <div className="project-row" >
-            <Project title={"Docs"} image={"/documentation.png"} language={"JS & CSS"} year={"2022"} href="https://github.com/PrathameshBhagat/Documentation-Project"/>
+            <Project title={"Stocks"} image={"/stock.jpg"} language={"Python & Java"} year={"2021-22"} href="https://github.com/PrathameshBhagat/Pratice-Files/tree/main/PPLab"/>
             <Project title={"Editor"} image={"/editor.png"} language={"JS & PHP"} year={"2022"} href="https://www.youtube.com/watch?feature=shared&v=OoUNVvhEFZg"/>
         </div>
     </div>
