55d6c8b65ace9a54cdb94562d27f13992906ac88
Your Name <you@example.com>
2026-04-19 17:54:47 +0530

    First try of footer, personal

---
+++ src/components/footer/footer.js
@@ -0,0 +1,87 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container"
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import {NavLink} from "react-router-dom";
+import './footer.css';
+import { Table } from "react-bootstrap";
+
+const Footer = () => {
+ 
+return (
+     <table striped hover className="bd-dark text-light">
+        <tbody>
+
+        <tr>
+            
+            <th>
+                Hello
+            </th>
+            <th>
+                Contact
+            </th>
+            <th>
+                Contact
+            </th>
+
+
+        </tr>
+        <tr>
+            
+            <td>
+                Hello
+            </td>
+            <td>
+                Contact
+            </td>
+            <td>
+                Contact
+            </td>
+
+
+        </tr>     <tr>
+            
+            <td>
+                Hello
+            </td>
+            <td>
+                Contact
+            </td>
+            <td>
+                Contact
+            </td>
+
+
+        </tr>     <tr>
+            
+            <td>
+                Hello
+            </td>
+            <td>
+                Contact
+            </td>
+            <td>
+                Contact
+            </td>
+
+
+        </tr>     <tr>
+            
+            <td>
+                Hello
+            </td>
+            <td>
+                Contact
+            </td>
+            <td>
+                Contact
+            </td>
+
+
+        </tr>
+        </tbody>
+    </table>
+  )
+}
+
+export default Footer
